c4cbf46a0ac3752252abe8874ab3f59762537dd3
Namibo06 <namibo654@gmail.com>
2024-02-20 15:08:07 -0300

    feat:blog

---
+++ app/src/main/java/com/gaat/blogapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.gaat.blogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isFirstTime();
+            }
+        },1500);
+    }
+
+    private void isFirstTime(){
+        SharedPreferences preferences = getApplication().getSharedPreferences("onboard", Context.MODE_PRIVATE);
+        boolean isFirstTime =  preferences.getBoolean("isFirstTime",true);
+
+        if(isFirstTime){
+            SharedPreferences.Editor editor =preferences.edit();
+            editor.putBoolean("isFirstTime",false);
+            editor.apply();
+
+            //start onboard activity
+            startActivity(new Intent(MainActivity.this,OnBoardActivity.class));
+            finish();
+        }else{
+            //start auth activity
+            startActivity(new Intent(MainActivity.this,AuthActivity.class));
+            finish();
+        }
+    }
+}
